8dbaef41533efe1c5f2ec83d8873ba5b958a01b9
Meng <75422140+mengzhu0308@users.noreply.github.com>
2020-12-28 14:17:49 +0800

    Update fpflu.py

--- Keras/fpflu.py
+++ Keras/fpflu.py
@@ -9,7 +9,10 @@
 from keras import backend as K
 import keras
 
-# https://doi.org/10.1016/j.neucom.2020.11.068
+'''
+https://doi.org/10.1016/j.neucom.2020.11.068
+PFLU and FPFLU: Two novel non-monotonic activation functions in convolutional neural networks
+'''
 def fpflu(x):
     return K.maximum(x, x / (1 + x * x))
 
